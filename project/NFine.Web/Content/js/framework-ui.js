864583ce0c34000a85089338cba32c7ff27aac7a
云行 邓 <271293794@qq.com>
2019-02-18 19:18:30 +0800

    修改framework-ui和连接字符串

--- project/NFine.Web/Content/js/framework-ui.js
+++ project/NFine.Web/Content/js/framework-ui.js
@@ -1,6 +1,7 @@
 ﻿$(function () {
     document.body.className = localStorage.getItem('config-skin');
     $("[data-toggle='tooltip']").tooltip();
+
 })
 $.reload = function () {
     location.reload();
@@ -24,6 +25,11 @@ $.loading = function (bool, text) {
     $loadingtext.css("left", (top.$('body').width() - $loadingtext.width()) / 2 - 50);
     $loadingtext.css("top", (top.$('body').height() - $loadingtext.height()) / 2);
 }
+/**
+ * 
+ * @param {any} name 从地址栏中要获取的参数
+ * @returns {String} 值
+ */
 $.request = function (name) {
     var search = location.search.slice(1);
     var arr = search.split("&");
@@ -89,7 +95,7 @@ $.modalOpen = function (options) {
         btnclass: ['btn btn-primary', 'btn btn-danger'],
         callBack: null
     };
-    var options = $.extend(defaults, options);
+    options = $.extend(defaults, options);
     var _width = top.$(window).width() > parseInt(options.width.replace('px', '')) ? options.width : top.$(window).width() + 'px';
     var _height = top.$(window).height() > parseInt(options.height.replace('px', '')) ? options.height : top.$(window).height() + 'px';
     top.layer.open({
@@ -170,6 +176,11 @@ $.modalClose = function () {
         location.reload();
     }
 }
+/**
+ * $.abc=function(){}，和 $.extend({abc:function(){}})效果一样
+ * 都是添加静态方法
+ * @param {any} options 选项
+ */
 $.submitForm = function (options) {
     var defaults = {
         url: "",
@@ -178,7 +189,7 @@ $.submitForm = function (options) {
         success: null,
         close: true
     };
-    var options = $.extend(defaults, options);
+    options = $.extend(defaults, options);
     $.loading(true, options.loading);
     window.setTimeout(function () {
         if ($('[name=__RequestVerificationToken]').length > 0) {
@@ -275,7 +286,7 @@ $.fn.jqGridRowValue = function () {
     if (selectedRowIds != "") {
         var json = [];
         var len = selectedRowIds.length;
-        for (var i = 0; i < len ; i++) {
+        for (var i = 0; i < len; i++) {
             var rowData = $grid.jqGrid('getRowData', selectedRowIds[i]);
             json.push(rowData);
         }
@@ -301,13 +312,19 @@ $.fn.formValid = function () {
         }
     });
 }
+/**
+ * 绑定或获取表格中的数据
+ * @param {Object} formdate 表格待绑定的数据
+ * @returns {Object} 表格中的数据
+ */
 $.fn.formSerialize = function (formdate) {
     var element = $(this);
-    if (!!formdate) {
+    if (formdate) {
         for (var key in formdate) {
             var $id = element.find('#' + key);
             var value = $.trim(formdate[key]).replace(/&nbsp;/g, '');
             var type = $id.attr('type');
+            // 使用select2插件后，看到的下拉框是span，真正的下拉框select是隐藏掉了
             if ($id.hasClass("select2-hidden-accessible")) {
                 type = "select";
             }
@@ -326,7 +343,7 @@ $.fn.formSerialize = function (formdate) {
                     $id.val(value);
                     break;
             }
-        };
+        }
         return false;
     }
     var postdata = {};
@@ -390,32 +407,96 @@ $.fn.bindSelect = function (options) {
         });
     }
 }
+/**
+ * 进入某个模块（菜单）时执行，不包括两个默认的页面
+ * 添加对象的方法
+ * */
 $.fn.authorizeButton = function () {
+    // 得到即将进入的模块的id
     var moduleId = top.$(".NFine_iframe:visible").attr("id").substr(6);
+    // 得到此模块上的按钮
     var dataJson = top.clients.authorizeButton[moduleId];
     var $element = $(this);
+    // 将此页面上所有的按钮都改为未授权状态。
     $element.find('a[authorize=yes]').attr('authorize', 'no');
-    if (dataJson != undefined) {
+    // 将在数据库存在对应记录的按钮的状态改为已授权
+    if (dataJson !== undefined) {
         $.each(dataJson, function (i) {
             $element.find("#" + dataJson[i].F_EnCode).attr('authorize', 'yes');
         });
     }
+    // 按钮中 a 标签的 id 都记录在数据库中，随意改变后，将会在数据库
+    // 中找不到此 id ，视为未授权，将被移除。
     $element.find("[authorize=no]").parents('li').prev('.split').remove();
     $element.find("[authorize=no]").parents('li').remove();
     $element.find('[authorize=no]').remove();
 }
+/**
+ * 从数据库创建按钮
+ * */
+$.fn.createButton = function () {
+    // 得到即将进入的模块的id
+    var moduleId = top.$(".NFine_iframe:visible").attr("id").substr(6);
+    // 得到此模块上的按钮
+    var dataJson = top.clients.authorizeButton[moduleId];
+    dataJson = dataJson.sort(function (a, b) {
+        return parseInt(a.F_SortCode) - parseInt(b.F_SortCode)
+    })
+
+    var $element = $(this);
+    if (dataJson !== undefined) {
+        $.each(dataJson, function (i) {
+            var eleLi = $("<li></li>")
+            let btnContainer;
+            // 按钮 fontawesome 图标
+            var eleI = $(`<i class="fa ${dataJson[i].F_Icon}"></i>`)
+            var eleA = $(`<a id="${dataJson[i].F_EnCode}" authorize="yes" data-url="${dataJson[i].F_UrlAddress}" onclick="${dataJson[i].F_JsEvent}">${dataJson[i].F_FullName}</a>`);
+            // eleA.attr("data-url", dataJson[i].F_UrlAddress);
+            eleA.prepend(eleI);
+            if (dataJson[i].F_Location === 1) {
+                // 如新建按钮，此类按钮忽略分隔符
+                btnContainer = $element.find(".btn-group:last");
+                eleA.addClass("btn btn-primary dropdown-text")
+                btnContainer.append(eleA);
+
+
+            } else {
+                btnContainer = $element.find(".nav")
+                eleLi.append(eleA);
+                btnContainer.append(eleLi)
+                if (dataJson[i].F_Split) {
+                    let eleSplit = $(`<li class="split"></li>`);
+                    btnContainer.append(eleSplit);
+                }
+            }
+        });
+    }
+
+
+
+}
+/**
+ * 
+ * @param {any} options 选项
+ */
 $.fn.dataGrid = function (options) {
     var defaults = {
         datatype: "json",
         autowidth: true,
+        // 是否显示行号
         rownumbers: true,
         shrinkToFit: false,
         gridview: true
     };
-    var options = $.extend(defaults, options);
+    options = $.extend(defaults, options);
     var $element = $(this);
+    // 行点击事件
     options["onSelectRow"] = function (rowid) {
-        var length = $(this).jqGrid("getGridParam", "selrow").length;
+        // 此条数据在当前页的行号，并非全局行号，和 rowid 相同，可替换
+        let rownumbers = $(this).jqGrid("getGridParam", "selrow")
+        var length = rownumbers.length;
+        // 获取此条数据内容使用 $(this).jqGrid('getRowData', rownumbers);
+        // 操作类按钮工具栏
         var $operate = $(".operate");
         if (length > 0) {
             $operate.animate({ "left": 0 }, 200);
@@ -427,4 +508,72 @@ $.fn.dataGrid = function (options) {
         })
     };
     $element.jqGrid(options);
+};
+/**
+ * 
+ * @param {Array} fields 每个对象为{id,name,type,option:[{value,name}]}的数组
+ * @param {JSON} setting 设置
+ */
+$.fn.makeTable = function (fields, setting) {
+    var $tr = $(`<tr></tr>`),
+        $table = ``;
+
+    var defaultSetting = {
+        areaValign: "top",// textarea中文字对齐方向
+        areaPaddingT: 5, // textarea的 padding-top
+        areaColspan: 3,
+        areaHeight: 60,
+        column: 2
+    };
+    setting = $.extend(defaultSetting, setting);
+
+    /**
+     * 检查给定的tr是否满
+     * @param {JQLite} tr 行
+     * @returns {boolean} 是否满
+     */
+    function isFull(tr) {
+        let children = tr.children(`td`);
+        if (children.length === 1 && children.get(0).children[0].tagName.toLowerCase() === `textarea`) {
+            return true;
+        } else if (children.length < setting.column) {
+            return false;
+        } else if (children.length > setting.column) {
+            throw new Error(`实际元素个数大于column`);
+        }
+
+        return true;
+
+    }
+    $table = $(`<table class="form"></table>`).append($tr),
+        $(this).append($table);
+
+    for (var i = 0; i < fields.length; i++) {
+        let optionsHtml = `<option value="">==请选择==</option>`;
+        if (fields[i].option) {
+
+            for (var index in fields[i].option) {
+                optionsHtml += `<option value="${fields[i].option[index].value}">${fields[i].option[index].name}</option>`;
+            }
+        }
+        fields[i].type = fields[i].type ? fields[i].type : `text`;
+        let $th = $(`<th class="formTitle" ${fields[i].type === "textarea" ? `valign="${setting.areaValign}" style="padding-top: ${setting.areaPaddingT}px;"` : ``}>${fields[i].name}</th>`);
+        let tagName = fields[i].type === `textarea` ? `textarea` : fields[i].type === `select` ? `select` : `input`;
+        let $td = $(`<td class="formValue" ${tagName === `textarea` ? `colspan="${setting.areaColspan}"` : ``}>
+                        <${tagName} id="${fields[i].id}" name="${fields[i].id}" class="form-control" ${tagName === `input` ? `type="${fields[i].type}"` : ``} ${tagName === `textarea` ? `style="height: ${setting.areaHeight}px;"` : ``} ${fields[i].placeholder ? `placeholder="${fields[i].placeholder}"` : ``}>${tagName === `select` ? optionsHtml : ""}</${tagName}>
+                    </td>`);
+
+
+
+
+        if (isFull($tr) || tagName === `textarea`) {
+            $tr = $(`<tr></tr>`);
+            $table.append($tr);
+        }
+        $tr.append($th);
+        $tr.append($td);
+    }
+
+    //生成 th td 组合
+
 };
